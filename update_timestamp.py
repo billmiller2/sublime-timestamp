e99935ad62789848bf566219483b0055b1cd4300
bill miller <billchill67@gmail.com>
2017-04-22 15:06:51 -0600

    tabs to spaces

--- update_timestamp.py
+++ update_timestamp.py
@@ -5,31 +5,31 @@ import time
 
 
 class UpdateTimestampCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		try:
-			original = self.view.file_name()
-			root, filename = os.path.split(original)
+    def run(self, edit):
+        try:
+            original = self.view.file_name()
+            root, filename = os.path.split(original)
 
-			if '_' in filename:
-				first = filename.split('_', 1)[0]
+            if '_' in filename:
+                first = filename.split('_', 1)[0]
 
-				if self.isNumeric(first) and len(first) == 10:
-					filename = filename.split('_', 1)[-1]
+                if self.isNumeric(first) and len(first) == 10:
+                    filename = filename.split('_', 1)[-1]
 
-			timestamp = str(time.time()).split('.', 1)[0]
-			updated = os.path.join(root, timestamp + '_' + filename)
-			os.rename(original, updated)
+            timestamp = str(time.time()).split('.', 1)[0]
+            updated = os.path.join(root, timestamp + '_' + filename)
+            os.rename(original, updated)
 
-			view = self.view.window().find_open_file(original)
-			if view:
-			    view.retarget(updated)
+            view = self.view.window().find_open_file(original)
+            if view:
+                view.retarget(updated)
 
-		except Exception as e:
-			sublime.status_message('Error: ' + str(e))
+        except Exception as e:
+            sublime.status_message('Error: ' + str(e))
 
-	def isNumeric(self, string):
-	    try: 
-	        int(string)
-	        return True
-	    except ValueError:
-	        return False
+    def isNumeric(self, string):
+        try: 
+            int(string)
+            return True
+        except ValueError:
+            return False
